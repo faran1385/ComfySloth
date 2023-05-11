a0e384b12b91548c037d8bef73d3721f8933f249
faran <hossinifaran@gmail.com>
2023-05-11 11:02:23 +0430

    add fetured product hover effect

--- src/Components/FeaturedProduct.js
+++ src/Components/FeaturedProduct.js
@@ -1,11 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import {useGlobalContextAPI} from "../context";
+import {BiSearchAlt2} from "react-icons/bi"
+import {useState} from "react";
 export function FeaturedProduct(props){
     const {name,id,price,image}=props.productProperties
-    if(name!==null){
+    const [featuredProductUnHovered,setFeaturedProductUnHovered]=useState(true)
+    if(name){
         return (
             <div className={"col-lg-4 mb-4"}>
-                <img src={image} style={{height:"14rem"}} className={"w-100 rounded-2"}/>
+                <div className={"position-relative"} onMouseLeave={()=>setFeaturedProductUnHovered(!featuredProductUnHovered)}
+                     onMouseEnter={()=>setFeaturedProductUnHovered(!featuredProductUnHovered)} style={{height:"14rem",cursor:"pointer"}}>
+                    <img  src={image} className={"h-100 featured-product-img w-100 rounded-2"}/>
+                    <div className={`position-absolute ${featuredProductUnHovered?"opacity-0":"opacity-100"} top-0 h-100 d-flex justify-content-center align-items-center start-0 rounded-2 w-100`}
+                         style={{background:"rgb(0,0,0,.6)",transition:"all 200ms ease-in"}}>
+                        <BiSearchAlt2 className={"text-white fs-1 rounded-circle p-1"} style={{background:"#ab7a5f"}}/>
+                    </div>
+                </div>
                 <div className={"d-flex justify-content-between mt-3"}>
                     <div className={"col-6"}>
                         <p style={{fontWeight:"400"}}>{name}</p>
