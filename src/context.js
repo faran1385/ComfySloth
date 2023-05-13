dc2e9b07c543d43ace0011075e530a63939478dd
faran <hossinifaran@gmail.com>
2023-05-13 12:09:42 +0430

    add a useFetch hook for fetch data

--- src/context.js
+++ src/context.js
@@ -1,15 +1,17 @@
 import React, {useContext, useEffect, useState} from "react";
 import axios from "axios";
+import {useFetch} from "./useFetch";
 
 const AppContext = React.createContext();
 export function AppProvider({children}) {
     const [featuredProduct,setFeaturedProduct]=useState([{id:0},{id:1},{id:2}])
-    async function fetchData(){
-        let res=await axios.get(`http://firstfact.freehost.io/`)
-        console.log(res.data)
+    async function FetchCaller(url){
+        let res=await useFetch(url)
+        console.log(res.data['-NV4ZgUFWaSxukmimVaa'])
+        setFeaturedProduct(res.data['-NV4ZgUFWaSxukmimVaa'])
     }
     useEffect(()=>{
-        fetchData()
+        FetchCaller('https://comfy-sloth-c3054-default-rtdb.firebaseio.com/FeaturedProducts.json')
     },[])
     return (
         <AppContext.Provider value={{featuredProduct}}>
